47d15a60657fc3d9986a2c2373226e0a0d2d5ebf
demidraksin <greatraksin@icloud.com>
2021-01-25 09:12:55 +0300

    Adding difficult conditions and homework solution✨

---
+++ lesson_2501/difficult_conditions.py
@@ -0,0 +1,19 @@
+'''a = True  # 1
+b = False  # 0
+
+# Если в переменной а содержится тру
+if a > b:
+    print('a is greater than b') <- будет это
+else:
+    print('a - ne True')
+'''
+ex1 = True
+ex2 = True
+ex3 = True
+ex4 = True
+mid = 4.5
+
+if (ex1 == ex2 == ex3 == ex4) and (mid > 4):
+    print('Passed!')
+else:
+    print('Not passed')
---
+++ lesson_2501/domashka.py
@@ -0,0 +1,7 @@
+age = int(input('Введите возраст: '))
+
+# if age >= 8 and age <= 21
+if 8 <= age <= 21:
+    print('ОК')
+else:
+    print('Ne OK')
