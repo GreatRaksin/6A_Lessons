7cc72414f2fcb958bccb19d7d26a0041b734e7b0
demidraksin <greatraksin@icloud.com>
2021-02-08 09:59:31 +0300

    Adding list sorting file🌸

--- lesson_0802/03_list_sorting.py
+++ lesson_0802/03_list_sorting.py
@@ -8,3 +8,5 @@ cars = ['toyota', 'audi', 'volvo', 'mercedes', 'bmw']
 print('Список ДО сортировки', cars)
 cars.sort()
 print('Список после сортировки', cars)
+cars.sort(reverse=True)
+print('Список после сортировки в обратном порядке', cars)
