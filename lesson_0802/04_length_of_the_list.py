aaa1b27b4408584a43b1008805eb5e438e0aceb0
demidraksin <greatraksin@icloud.com>
2021-02-08 10:07:28 +0300

    Lists length measuring🐚

---
+++ lesson_0802/04_length_of_the_list.py
@@ -0,0 +1,9 @@
+'''
+Определение длины списка происходит с помощью функции
+len()
+'''
+
+cars = ['toyota', 'audi', 'volvo', 'mercedes', 'bmw']
+motorcycles = ['cf motors', 'bmw', 'yamaha', 'ducati', 'suzuki', 'm1nsk']
+print('Длина списка cars', len(cars))
+print('Длина списка motorcycles', len(motorcycles))
