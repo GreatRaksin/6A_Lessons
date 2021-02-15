bc6e94d8ee17242394c14feb4248b6dfdd850915
demidraksin <greatraksin@icloud.com>
2021-02-15 09:44:17 +0300

    min, max, sum functions explanation🍀

---
+++ lesson_1502/3_statisctics_function.py
@@ -0,0 +1,4 @@
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print('Сумма всех чисел в списке sum():', sum(numbers))
+print('Максимальное число в списке max():', max(numbers))
+print('Минимальное число в списке min():', min(numbers))
