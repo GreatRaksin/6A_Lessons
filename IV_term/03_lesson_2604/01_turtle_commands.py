23b29ed701f56daa27ad2d5f49f42c564ce4ae24
demidraksin <greatraksin@icloud.com>
2021-04-26 09:42:06 +0300

    Adding new turtle commands🔒

--- IV_term/03_lesson_2604/01_turtle_commands.py
+++ IV_term/03_lesson_2604/01_turtle_commands.py
@@ -2,15 +2,22 @@ from turtle import *
 
 tina = Turtle()  # создал черепашку
 tina.shape('turtle')  # указал, что форма будет черепашкой
+tina.speed(5)  # скорость черепашки. от 1 до 10
 
+tina.color('yellow')  # указываю код цвета для черепашки
+tina.pensize(10)  # отвечает за размер линии у черепашки
 tina.fd(100)
 # fd(length) - forward(length) идти вперед
 tina.rt(45)
 # rt(degree) - right(degree) повернуть вправо на degree градусов
+tina.color('violet')
+tina.pensize(5)
 tina.bk(50)
 # bk(length) - backward(length) идти назад
+tina.color('#3eb489')
 tina.lt(90)
 # lt(degree) - left(degree) повернуть влево на degree градусов
+tina.pensize(1)
 tina.bk(100)
 
 done()  # не даю окну закрыться сразу
