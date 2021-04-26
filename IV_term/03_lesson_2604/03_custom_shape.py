4cee6c9e8c86404c6598ab03a011e88e5c995e20
demidraksin <greatraksin@icloud.com>
2021-04-26 10:24:27 +0300

    Adding random choice color☂️

--- IV_term/03_lesson_2604/03_custom_shape.py
+++ IV_term/03_lesson_2604/03_custom_shape.py
@@ -1,4 +1,5 @@
 from turtle import *
+from random import choice
 
 tina = Turtle()  # создал черепашку
 s = Screen()  # создаю переменную экрана
@@ -8,11 +9,16 @@ register_shape('turtle.gif')  # регистрация новой формы
 tina.shape('turtle.gif')  # применение новой формы
 tina.color('#ffffff')
 
-
-tina.fd(100)
-tina.lt(90)
-tina.fd(100)
-
-# flaticon.com
+colors = ['#ffff00', '#666600', '#e09d3f', '#ffc0cb',
+          '#fa9284', '#45f235', '#d8ebd1', '#ff00ff', '#42aaff']
+step = 10  # сколько шагов идти
+size = 2  # размер пера
+for side in range(400):
+    tina.color(choice(colors))
+    tina.fd(step)
+    tina.left(90)  # если менять угол, будут получаться красивые штуки
+    tina.pensize(size)
+    step += 10
+    size += 0.01
 
 done()  # не даю окну закрыться сразу
